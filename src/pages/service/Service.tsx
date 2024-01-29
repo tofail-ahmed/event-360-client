538955d16c1be2d7b282e2f7ce01284e1215a7de
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-29 23:09:12 +0600

    tailwindcss  added

--- src/pages/service/Service.tsx
+++ src/pages/service/Service.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <div>service</div>
+    <div>
+      <h1 className=' text-center text-4xl text-red-500'>This is service page</h1>
+    </div>
   )
 }
 
