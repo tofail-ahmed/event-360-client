ba280ded37cfb126d192d53ba25151cae987ed41
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-30 13:25:14 +0600

    valuable client section initiated

---
+++ src/pages/home/Client/Client.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Client.css'
+import Container from '../../../component/Container'
+
+const Client = () => {
+  return (
+    <Container className='clientBg'>
+      <h1 className=''>Our Valuable client</h1>
+    </Container>
+  )
+}
+
+export default Client
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Header'
 import '../home/Home.css'
+import Client from './Client/Client'
 const Home = () => {
   return (
     <div className='homeBg'>
       <Navbar></Navbar>
-      
+      <Client></Client>
     </div>
   )
 }
