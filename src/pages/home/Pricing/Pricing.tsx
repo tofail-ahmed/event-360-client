e670af4ec51663f6e0df05af84e1006930641f10
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-01 21:33:59 +0600

    pricing plan done

--- src/pages/home/Pricing/Pricing.tsx
+++ src/pages/home/Pricing/Pricing.tsx
@@ -4,13 +4,16 @@ import './Pricing.css'
 import PricingCard from './PricingCard/PricingCard'
 const Pricing = () => {
   return (
-    <Container>
-      <div>
-            <h1 className='priceHead'>Explore our pricing plans</h1>
-            <p className='priceDescrp max-w-[1080px] mx-auto'>Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.</p>
-            <div className='grid lg:grid-cols-3 gap-5 grid-cols-1'>
+    <Container className='py-16'>
+      <div >
+            <h1 className='priceHead my-6'>Explore our pricing plans</h1>
+            <p className='priceDescrp lg:max-w-[1080px] max-w-[520px] mx-auto my-6'>Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.</p>
+           
+
+            <div className='grid lg:grid-cols-3 gap-8 grid-cols-1 mt-24'>
               <PricingCard></PricingCard>
-              <PricingCard></PricingCard>
+            <div className='relative '>   <p className="bg-blue-600 text-white px-2 py-0.5 rounded-t-lg absolute z-0 -top-7 lg:left-[30%] left-[38%] flex">RECOMMENDED</p><div className='z-50'>
+            <PricingCard></PricingCard></div></div>
               <PricingCard></PricingCard>
             </div>
       </div>
--- src/pages/home/Pricing/PricingCard/PricingCard.tsx
+++ src/pages/home/Pricing/PricingCard/PricingCard.tsx
@@ -1,11 +1,64 @@
 import React from "react";
-import { FaBorderAll } from "react-icons/fa";
+import { FaBorderAll, FaArrowRight, FaCheck, FaCodepen, FaHive, FaRocket } from "react-icons/fa";
 const PricingCard = () => {
   return (
-    <div className=" bg-white p-5 rounded-md border-t-2 border-[#df4e9d]">
-      <div className="flex justify-between items-center text-4xl font-bold p-5 text-[#df4e9d]">
-        <div ><FaBorderAll /></div>
+    <div className=" bg-white p-5 rounded-md border-t-4  border-[#df4e9d] lg:mx-0 mx-14 ">
+      <div className="flex justify-between items-center text-4xl font-bold text-[#df4e9d]">
+        <div className="bg-[#df4e9e49] p-4 rounded-md mb-5 text-md rotate-0">
+        {/* <FaCodepen /> */}
+        {/* <FaHive /> */}
+        <FaRocket />
+        </div>
         <p>$21</p>
+      </div>
+     
+      <div className="mt-5">
+        <h1 className="text-xl font-bold my-5">Standard</h1>
+        <p className="mb-5">
+          Upgrade your social portfolio with a stunning profile & enhanced
+          shots.
+        </p>
+        <button className="flex items-center gap-3 mb-6 text-blue-600 border-2 border-blue-600 rounded-md py-1 px-2 hover:text-white hover:bg-blue-600">
+          Get This Package <FaArrowRight />
+        </button>
+      </div>
+      <hr />
+      <div className="mt-8">
+        <div className="flex items-center gap-2 my-2 line-through opacity-50">
+          
+          <span className="text-green-600 text-sm bg-green-400/50 p-2 rounded-[50%]">
+            <FaCheck />
+          </span>
+          <p>User Dashboard</p>
+        </div>
+        <s className="flex items-center gap-2 my-2">
+          
+          <span className="text-green-600 text-sm bg-green-400/50 p-2 rounded-[50%]">
+            <FaCheck />
+          </span>
+          <p>User Dashboard</p>
+        </s>
+        <div className="flex items-center gap-2 my-2">
+          
+          <span className="text-green-600 text-sm bg-green-400/50 p-2 rounded-[50%]">
+            <FaCheck />
+          </span>
+          <p>User Dashboard</p>
+        </div>
+        <div className="flex items-center gap-2 my-2">
+          
+          <span className="text-green-600 text-sm bg-green-400/50 p-2 rounded-[50%]">
+            <FaCheck />
+          </span>
+          <p>User Dashboard</p>
+        </div>
+        <div className="flex items-center gap-2 my-2">
+          
+          <span className="text-green-600 text-sm bg-green-400/50 p-2 rounded-[50%]">
+            <FaCheck />
+          </span>
+          <p>User Dashboard</p>
+        </div>
       </div>
     </div>
   );
