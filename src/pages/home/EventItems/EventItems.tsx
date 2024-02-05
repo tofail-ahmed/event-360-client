904d6399be7cd55deb45138b506a692a6a7356b4
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-05 23:20:01 +0600

    dynamic eventItems/home done

--- src/pages/home/EventItems/EventItems.tsx
+++ src/pages/home/EventItems/EventItems.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Container from "../../../component/Container";
 import event_1 from "../../../assets/event_1.png";
-import event_2 from "../../../assets/event_2.png";
-import event_3 from "../../../assets/event_3.png";
-import event_4 from "../../../assets/event_4.png";
-import event_5 from "../../../assets/event_5.png";
-import event_6 from "../../../assets/event_6.png";
+
 import './EventItems.css'
+import { useQuery } from "@tanstack/react-query";
 const EventItems = () => {
+  const fetchData=async()=>{
+    const res=await fetch(`http://localhost:5000/eventItems`);
+    const data=await res.json();
+    return data;
+  }
+  const {data,error, isLoading}=useQuery({
+    queryKey:["eventItems"],
+    queryFn:fetchData
+  })
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <Container >
       <div className="text-center my-16">
@@ -17,33 +30,33 @@ const EventItems = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 ">
         <div className="grid grid-rows-2 mx-auto gap-5">
           <div className="bg-white/40 p-6 rounded-md">
-            <img className="w-[280px] h-[140px]"  src={event_1} alt="" />
-            <p className="font-bold">Event Item-1</p>
+            <img className="w-[280px] h-[140px]"  src={data[0].image} alt="" />
+            <p className="font-bold">{data[0].eventItem}</p>
           </div>
           <div className="flex gap-5">
             <div className="bg-white/40 p-6 rounded-md">
-              <img className="w-[107px] h-[140px]"  src={event_1} alt="" />
-              <p className="font-bold">Event Item-2</p>
+              <img className="w-[107px] h-[140px]"  src={data[1].image} alt="" />
+              <p className="font-bold">{data[1].eventItem.slice(0,9)}</p>
             </div>
             <div className="bg-white/40 p-6 rounded-md">
-              <img className="w-[107px] h-[140px]"  src={event_1} alt="" />
-              <p className="font-bold">Event Item-3</p>
+              <img className="w-[107px] h-[140px]"  src={data[2].image} alt="" />
+              <p className="font-bold">{data[2].eventItem.slice(0,9)}</p>
             </div>
           </div>
         </div>
         <div className="mx-auto">
           <div className="bg-white/40 p-6 rounded-md mb-7">
-            <img className="w-[280px] h-[140px]" src={event_1} alt="" />
-            <p className="font-bold">Event Item-4</p>
+            <img className="w-[280px] h-[140px]" src={data[3].image} alt="" />
+            <p className="font-bold">{data[3].eventItem}</p>
           </div>
           <div className="bg-white/40 p-6 rounded-md">
-            <img className="w-[280px] h-[140px]" src={event_1} alt="" />
-            <p className="font-bold">Event Item-5</p>
+            <img className="w-[280px] h-[140px]" src={data[4].image}alt="" />
+            <p className="font-bold">{data[4].eventItem}</p>
           </div>
         </div>
         <div className="mx-auto bg-white/40 p-6 rounded-md">
-          <img className="w-[283px] h-[375px]" src={event_1} alt="" />
-          <p className="font-bold">Event Item-6</p>
+          <img className="w-[283px] h-[375px]"src={data[5].image} alt="" />
+          <p className="font-bold">{data[5].eventItem}</p>
         </div>
       </div>
      
